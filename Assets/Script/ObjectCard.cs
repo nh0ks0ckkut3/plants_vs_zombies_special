e2b92afe8c83725352fcf29ff5f803e97d4ac30c
nh0ks0ckkut3 <bason1607@gmail.com>
2023-11-12 13:09:51 +0700

    '12112023'

---
+++ Assets/Script/ObjectCard.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObjectCard : MonoBehaviour,  IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    public GameObject object_Drag;
+    public GameObject object_Game;
+    public Canvas canvas;
+    private GameObject objectDragInstance;
+
+    private Transform objectDragTransform;
+    private CanvasGroup canvasGroup;
+
+    private game_manager gameManager;
+
+    public void Start()
+    {
+        gameManager = game_manager.instance;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Cập nhật vị trí của objectDragInstance theo vị trí con trỏ chuột
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        objectDragTransform.position = mousePosition;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        objectDragInstance = Instantiate(object_Drag, Vector3.zero, Quaternion.identity);
+        objectDragTransform = objectDragInstance.GetComponent<Transform>();
+        canvasGroup = objectDragInstance.AddComponent<CanvasGroup>();
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0; // Đảm bảo đối tượng nằm trong mặt phẳng XY
+
+       
+
+        // Đặt vị trí của objectDragInstance tại vị trí con trỏ chuột
+        objectDragInstance.transform.position = mousePosition;
+
+        //
+        objectDragInstance.GetComponent<Object_Dragging>().card = this;
+
+        // Đảm bảo đối tượng mới tạo nằm trên cùng lớp render
+        objectDragInstance.transform.SetAsLastSibling();
+        
+         // Kích hoạt tính năng tương tác sau khi kéo đối tượng
+        canvasGroup.blocksRaycasts = true;
+
+        //Debug.Log("" + mousePosition + " " + objectDragInstance.transform);
+        gameManager.draggingObject = objectDragInstance;
+
+    }
+    //objectDragInstance.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    //objectDragInstance = Instantiate(object_Drag,new Vector3(-8, 0, 0), Quaternion.identity);
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        gameManager.placeObject();
+        gameManager.draggingObject = null;
+        Destroy(objectDragInstance);
+        
+    }
+}
---
+++ Assets/Script/game_manager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_manager : MonoBehaviour
+{
+    public GameObject draggingObject;
+    public GameObject currentContainer;
+
+    public static game_manager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void placeObject()
+    {
+        if (draggingObject != null && currentContainer != null)
+        {
+            
+            GameObject gameObject = draggingObject.GetComponent<Object_Dragging>().card.object_Game;
+            
+            Instantiate(gameObject, currentContainer.transform);
+            // Đảm bảo đối tượng mới tạo nằm trên cùng lớp render
+            gameObject.transform.SetAsLastSibling();
+            gameObject.transform.position = new Vector3(0, 0.2f, currentContainer.transform.position.z);
+
+
+            Debug.Log(currentContainer.transform.position);
+            Debug.Log(currentContainer.transform.localPosition);
+            Debug.Log(gameObject.transform.position);
+            Debug.Log(gameObject.transform.localPosition);
+            currentContainer.GetComponent<ObjectContainer>().isFull = true;
+        }
+    }
+}
